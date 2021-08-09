e078b18485a93011bb301282372a71122a29583d
GitHubSeob <mnrz100@naver.com>
2021-08-09 23:17:19 +0900

    백준/11053/김진섭

---
+++ 2021_08/week_2/KJS_11053.cpp
@@ -0,0 +1,27 @@
+﻿//문제풀이: https://githubseob.tistory.com/17
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+	vector<int>Arr(N, 0);
+	vector<int>DP(N, 1);
+	int y = 0, x = 0;
+	for (x = 0; x < N; ++x) {
+		cin >> Arr[x];
+	}
+	int answer = 0;
+	for (y = 0; y < N; ++y) {
+		for (x = 0; x < y;++x) {
+			if (Arr[x] < Arr[y]) {
+				DP[y] = max(DP[y], DP[x] + 1);
+			}
+		}
+		answer = max(answer, DP[y]);
+	}
+	cout << answer;
+}
