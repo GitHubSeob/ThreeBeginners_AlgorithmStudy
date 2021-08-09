e4f82b609693b8159168c7d2e4f4b7e6b2e1e99e
GitHubSeob <mnrz100@naver.com>
2021-08-09 22:29:51 +0900

    백준/2193/김진섭

---
+++ 2021_08/week_2/KJS_2193.cpp
@@ -0,0 +1,18 @@
+﻿//문제풀이: https://githubseob.tistory.com/15
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+	long long DP[91] = { 0, };
+	DP[0] = DP[1] = DP[2] = 1;
+
+	for (int i = 3; i <= N; ++i) {
+		for (int j = 0; j <= i - 2; ++j) {
+			DP[i] += DP[j];
+		}
+	}		
+	cout << DP[N];
+}
