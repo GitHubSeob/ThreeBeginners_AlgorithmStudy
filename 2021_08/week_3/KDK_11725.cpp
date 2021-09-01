c045503b4764c23d09f59b9b0a792d3745424eee
KJS <83795383+GitHubSeob@users.noreply.github.com>
2021-09-01 20:46:20 +0900

    백준/11725/김동균

--- 2021_08/week_3/KDK_11725.cpp
+++ 2021_08/week_3/KDK_11725.cpp
@@ -1,46 +1,46 @@
-//#include <vector>
-//#include <string>
-//#include <queue>
-//#include <iostream>
-//
-//using namespace std;
-//
-//bool chk[100001];
-//vector<int> graph[100001]; // »ç¿ë ÀÎµ¦½º : 1~100000
-//
-//int main() {
-//	int n; // ³ëµåÀÇ °³¼ö
-//	cin >> n;
-//
-//
-//	for (int i = 0; i < n - 1; i++) {
-//		int a, b;
-//		cin >> a >> b;
-//		graph[a].push_back(b);
-//		graph[b].push_back(a);
-//	}
-//
-//	// BFS Å½»ö
-//	int* answer = new int[n + 1]; // »ç¿ë ÀÎµ¦½º : 1~n
-//
-//	queue<int> q;
-//	q.push(1);
-//	chk[1] = true;
-//
-//	while (!q.empty()) {
-//		int x = q.front();
-//		q.pop();
-//
-//		for (int i = 0; i < graph[x].size(); i++) {
-//			if (!chk[graph[x][i]]) {
-//				q.push(graph[x][i]);
-//				chk[graph[x][i]] = true;
-//				answer[graph[x][i]] = x;
-//			}
-//		}
-//	}
-//
-//	for (int i = 2; i <= n; i++) {
-//		cout << answer[i] << '\n';
-//	}
-//}
+#include <vector>
+#include <string>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+bool chk[100001];
+vector<int> graph[100001]; // ì‚¬ìš© ì¸ë±ìŠ¤ : 1~100000
+
+int main() {
+	int n; // ë…¸ë“œì˜ ê°œìˆ˜
+	cin >> n;
+
+
+	for (int i = 0; i < n - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+
+	// BFS íƒìƒ‰
+	int* answer = new int[n + 1]; // ì‚¬ìš© ì¸ë±ìŠ¤ : 1~n
+
+	queue<int> q;
+	q.push(1);
+	chk[1] = true;
+
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+
+		for (int i = 0; i < graph[x].size(); i++) {
+			if (!chk[graph[x][i]]) {
+				q.push(graph[x][i]);
+				chk[graph[x][i]] = true;
+				answer[graph[x][i]] = x;
+			}
+		}
+	}
+
+	for (int i = 2; i <= n; i++) {
+		cout << answer[i] << '\n';
+	}
+}
