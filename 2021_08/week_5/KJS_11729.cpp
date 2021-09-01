d9364fdb1082dd51bd9af9b744c29509af7d0253
GitHubSeob <mnrz100@naver.com>
2021-09-02 01:04:41 +0900

    백준/11729/김진섭

---
+++ 2021_08/week_5/KJS_11729.cpp
@@ -0,0 +1,25 @@
+﻿//문제풀이: https://githubseob.tistory.com/69
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void hanoi(int N, char start, char via, char end) {
+	if (N == 1) cout << start << ' ' << end << '\n';
+	else
+	{
+		hanoi(N - 1, start, end, via);
+		cout  << start << ' ' << end << '\n';
+		hanoi(N - 1, via, start, end);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N = 0;
+	cin >> N;
+	cout << (int)pow(2, N) - 1 << '\n';
+	hanoi(N, '1', '2', '3');
+}
