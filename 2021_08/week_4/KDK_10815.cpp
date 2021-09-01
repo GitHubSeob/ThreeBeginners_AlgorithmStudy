d35f1364e995bb8c72c20a9e6b6d58d969057cfb
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2021-09-01 18:28:12 +0900

    백준/10815/김동균

---
+++ 2021_08/week_4/KDK_10815.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//#include<cstring>
+//
+//
+//using namespace std;
+//
+//long long arr[10001];
+//
+//long long cnt = 0;
+//
+//
+//int main() {
