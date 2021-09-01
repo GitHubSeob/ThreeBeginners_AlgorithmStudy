25da1e7e51319c07ee5af02d8f310bb3e895c6f0
KJS <83795383+GitHubSeob@users.noreply.github.com>
2021-09-01 20:49:00 +0900

    백준/2110/김동균

--- 2021_08/week_4/KDK_2110.cpp
+++ 2021_08/week_4/KDK_2110.cpp
@@ -1,34 +1,34 @@
-//#include <iostream>
-//#include <algorithm>
-//#include <vector>
-//using namespace std;
-//
-//int main() {
-//
-//	int N = 0, C = 0;
-//	
-//
-//	cin >> N >> C;
-//
-//	vector<int>house;
-//	for (int i=0; i < N; ++i)
-//		cin >> house[i];
-//	sort(house.begin(), house.end());                       //Á¤·Ä
-//
-//	int left = 0, right = house[N - 1];
-//
-//	while (left <= right) {
-//		int wifi = 1;
-//		int pre_idx = 0;
-//		int mid = (left + right) / 2;
-//		for (int i = 1; i < N; i++) {
-//			if (house[i] - house[i-1] >= mid) {
-//				wifi++;
-//			}
-//		}
-//		if (wifi >= C)
-//			left = mid + 1;
-//		else right = mid - 1;
-//	}
-//	cout << right;
-//}
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	int N = 0, C = 0;
+	
+
+	cin >> N >> C;
+
+	vector<int>house;
+	for (int i=0; i < N; ++i)
+		cin >> house[i];
+	sort(house.begin(), house.end());                       
+
+	int left = 0, right = house[N - 1];
+
+	while (left <= right) {
+		int wifi = 1;
+		int pre_idx = 0;
+		int mid = (left + right) / 2;
+		for (int i = 1; i < N; i++) {
+			if (house[i] - house[i-1] >= mid) {
+				wifi++;
+			}
+		}
+		if (wifi >= C)
+			left = mid + 1;
+		else right = mid - 1;
+	}
+	cout << right;
+}
