ee1112b7f9401d1fe5c1f96936cb064d5c927025
KJS <83795383+GitHubSeob@users.noreply.github.com>
2021-09-01 20:47:27 +0900

    백준/1991/김동균

--- 2021_08/week_3/KDK_1991.cpp
+++ 2021_08/week_3/KDK_1991.cpp
@@ -1,100 +1,55 @@
-//#include <iostream>
-//#include<vector> 
-//using namespace std;
-//int N;
-//char P, L, R;
-//int parent[26][2];
-//typedef struct treeRecord {
-//	char Id;
-//	struct treeRecord* LChild;
-//	struct treeRecord* RChild;
-//}node;
-//typedef node* Nptr;
-//node nodes[27];
-//void PreOrder(Nptr T)
-//{
-//	if (T != NULL) {
-//		cout << T->Id;
-//		PreOrder(T->LChild);
-//		PreOrder(T->RChild);
-//	}
-//}
-//
-//void InOrder(Nptr T) {
-//	if (T != NULL) {
-//		cout << T->Id;
-//		InOrder(T->LChild);
-//		InOrder(T->RChild);
-//	}
-//}
-//void PostOrder(Nptr T) {
-//	if (T != NULL) {
-//		cout << T->Id;
-//		PostOrder(T->LChild);
-//		PostOrder(T->RChild);
-//	}
-//}
-//int main(void) {
-//	int n;
-//	cin >> n;
-//	treeRecord M;
-//	for (int i = 0; i < n; i++) {
-//		cin >> M.Id;
-//	}
-//	PreOrder(M)
-//}
-//#include <iostream>
-//using namespace std;
-//
-//struct node {
-//    char LChild; // ¿ŞÂÊ ÀÚ½Ä
-//    char RChild; // ¿À¸¥ÂÊ ÀÚ½Ä
-//};
-//
-//node arr[27];
-//
-//void preorder(char a) { // ÀüÀ§ ¼øÈ¸
-//    if (a != '.') {
-//        cout << a; 
-//        preorder(arr[a].LChild);
-//        preorder(arr[a].RChild);
-//    }
-//    else return; 
-//}
-//
-//void inorder(char a) { // ÁßÀ§ ¼øÈ¸
-//    if (a != '.') {
-//        inorder(arr[a].LChild);
-//        cout << a; 
-//        inorder(arr[a].RChild);
-//    }
-//    else return; 
-//}
-//
-//void postorder(char a) { // ÈÄÀ§ ¼øÈ¸
-//    if (a != '.') {
-//        postorder(arr[a].LChild); 
-//        postorder(arr[a].RChild); 
-//        cout << a; 
-//    }
-//    else return; 
-//}
-//
-//int main() {
-//    int n;
-//    char a, b, c;
-//
-//    cin >> n;
-//    for (int i = 1; i < n + 1; i++) {
-//        cin >> a >> b >> c;
-//        arr[a].LChild = b; 
-//        arr[a].RChild = c; 
-//    }
-//
-//    preorder('A'); // Ç×»ó A°¡ ·çÆ® ³ëµå
-//    cout << "\n";
-//    inorder('A');
-//    cout << "\n";
-//    postorder('A');
-//    cout << "\n";
-//}
+#include <iostream>
+using namespace std;
+
+struct node {
+    char LChild; // ì™¼ìª½ ìì‹
+    char RChild; // ì˜¤ë¥¸ìª½ ìì‹
+};
+
+node arr[27];
+
+void preorder(char a) { // ì „ìœ„ ìˆœíšŒ
+    if (a != '.') {
+        cout << a; 
+        preorder(arr[a].LChild);
+        preorder(arr[a].RChild);
+    }
+    else return; 
+}
+
+void inorder(char a) { // ì¤‘ìœ„ ìˆœíšŒ
+    if (a != '.') {
+        inorder(arr[a].LChild);
+        cout << a; 
+        inorder(arr[a].RChild);
+    }
+    else return; 
+}
+
+void postorder(char a) { // í›„ìœ„ ìˆœíšŒ
+    if (a != '.') {
+        postorder(arr[a].LChild); 
+        postorder(arr[a].RChild); 
+        cout << a; 
+    }
+    else return; 
+}
+
+int main() {
+    int n;
+    char a, b, c;
+
+    cin >> n;
+    for (int i = 1; i < n + 1; i++) {
+        cin >> a >> b >> c;
+        arr[a].LChild = b; 
+        arr[a].RChild = c; 
+    }
+
+    preorder('A'); // í•­ìƒ Aê°€ ë£¨íŠ¸ ë…¸ë“œ
+    cout << "\n";
+    inorder('A');
+    cout << "\n";
+    postorder('A');
+    cout << "\n";
+}
