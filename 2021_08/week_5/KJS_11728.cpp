67f6913a85da0c9edb2c78216b6de89173641042
GitHubSeob <mnrz100@naver.com>
2021-09-01 20:52:14 +0900

    백준/11728/김진섭

---
+++ 2021_08/week_5/KJS_11728.cpp
@@ -0,0 +1,27 @@
+﻿//문제풀이: https://githubseob.tistory.com/56
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A_cnt = 0;
+	int B_cnt = 0;
+	int idx = 0;
+
+	cin >> A_cnt;
+	cin >> B_cnt;
+	vector<int>Arr(A_cnt + B_cnt, 0);
+
+	for (int idx = 0; idx < A_cnt + B_cnt; ++idx)
+		cin >> Arr[idx];
+
+	sort(Arr.begin(), Arr.end());
+
+	for (idx = 0; idx < Arr.size(); ++idx)
+		cout << Arr[idx] << " ";
+}
