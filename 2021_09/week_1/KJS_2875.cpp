46e98298e7939d878ffe2c3b69cddcda6a06dfa4
GitHubSeob <mnrz100@naver.com>
2021-09-05 22:34:09 +0900

    백준/2875/김진섭

---
+++ 2021_09/week_1/KJS_2875.cpp
@@ -0,0 +1,21 @@
+﻿//문제풀이: https://githubseob.tistory.com/71
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N = 0, M = 0, K = 0;	
+	cin >> N >> M >> K;
+
+	int team = min(N / 2, M);
+	int alone = N + M - (3 * team);
+	
+	K -= alone;
+	
+	if (K <= 0) cout << team;	
+	else cout << team - (K + 2) / 3;
+}
