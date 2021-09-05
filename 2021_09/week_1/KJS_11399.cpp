c00f77d15be3d3e392467a604edba8957cf3ea2a
GitHubSeob <mnrz100@naver.com>
2021-09-05 22:33:51 +0900

    백준/11399/김진섭

---
+++ 2021_09/week_1/KJS_11399.cpp
@@ -0,0 +1,30 @@
+﻿//문제풀이: https://githubseob.tistory.com/75
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N = 0;
+	int idx = 0;
+	int acc = 0;
+	int answer = 0;
+
+	cin >> N;
+	vector<int>time(N, 0);
+
+	for (idx = 0; idx < N; ++idx)
+		cin >> time[idx];
+	sort(time.begin(), time.end());
+
+	for (idx = 0; idx < N; ++idx) {
+		acc += time[idx];
+		answer += acc;
+	}
+	cout << answer;
+}
