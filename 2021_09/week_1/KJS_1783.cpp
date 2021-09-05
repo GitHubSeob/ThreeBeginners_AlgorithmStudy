5e571056affb30e44b76e30e23ba28e73367dec7
GitHubSeob <mnrz100@naver.com>
2021-09-05 22:33:55 +0900

    백준/1783/김진섭

---
+++ 2021_09/week_1/KJS_1783.cpp
@@ -0,0 +1,22 @@
+﻿//문제풀이: https://githubseob.tistory.com/73
+
+#include <iostream>
+using namespace std;
+
+int main() {	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N = 0, M = 0;
+	cin >> N >> M;
+
+	if (N == 1) cout << 1;
+	else if (N == 2) {
+		if (M < 7)	cout << (M + 1) / 2;
+		else cout << 4;
+	}
+	else {
+		if (M <= 4) cout << M;
+		else if (M >= 7) cout << M - 2;
+		else cout << 4;
+	}
+}
