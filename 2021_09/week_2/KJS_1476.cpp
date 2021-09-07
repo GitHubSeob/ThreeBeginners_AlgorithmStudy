4dcb5acdec33864338d93c0498c90ca54f7f5402
GitHubSeob <mnrz100@naver.com>
2021-09-08 02:19:14 +0900

    백준/1476/김진섭

---
+++ 2021_09/week_2/KJS_1476.cpp
@@ -0,0 +1,19 @@
+﻿//문제풀이: https://githubseob.tistory.com/86
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int E, S, M = 0;
+	cin >> E >> S >> M;
+
+	int idx = 0;
+	int year = 0;
+	while (1) {
+		if ((year % 15) + 1 == E && (year % 28) + 1 == S && (year % 19) + 1 == M) {
+			cout << year + 1;
+			break;
+		}
+		else year++;
+	}
+}
