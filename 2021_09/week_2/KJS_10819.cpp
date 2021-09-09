eea72dce977a2d869d7e6c8241cff724837730d9
GitHubSeob <mnrz100@naver.com>
2021-09-09 18:24:09 +0900

    백준/10819/김진섭

---
+++ 2021_09/week_2/KJS_10819.cpp
@@ -0,0 +1,28 @@
+﻿//문제풀이: https://githubseob.tistory.com/89
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N = 0;
+	int idx = 0;
+	cin >> N;
+	vector<int>arr(N, 0);
+	for (idx = 0; idx < N; ++idx)
+		cin >> arr[idx];
+	sort(arr.begin(), arr.end());
+
+	int answer = 0;
+	int sum = 0;
+	do {
+		sum = 0;
+		for (idx = 0; idx < N-1; ++idx)
+			sum += abs(arr[idx] - arr[idx + 1]);		
+		answer = max(answer, sum);
+	}while(next_permutation(arr.begin(), arr.end()));
+
+	cout << answer;
+}
