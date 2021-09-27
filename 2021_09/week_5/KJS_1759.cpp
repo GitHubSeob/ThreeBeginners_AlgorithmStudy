dfd8dac002353b8154ad13cb859a3ff46622dd50
GitHubSeob <mnrz100@naver.com>
2021-09-27 22:45:43 +0900

    백준/1759/김진섭

---
+++ 2021_09/week_5/KJS_1759.cpp
@@ -0,0 +1,47 @@
+﻿//문제풀이: https://githubseob.tistory.com/120
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	string word = "";
+	string code = "";
+	int L(0), C(0);
+	int consonant(0), vowel(0);
+	int idx(0), idx2(0);
+	set<string>answer;
+	set<string>::iterator it;
+	cin >> L >> C;
+	char character = ' ';
+	for (idx = 0; idx < C; ++idx) {
+		cin >> character;
+		word += character;
+	}
+
+	sort(word.begin(), word.end());
+	
+	for (idx = 1; idx < (1 << C); ++idx) {
+		consonant = 0;
+		vowel = 0;
+		code = "";
+		for (idx2 = 0; idx2 < C; ++idx2) {
+			if (idx & (1 << idx2)) {
+				cout << idx2 << ' ';
+				if (word[idx2] == 'a' || word[idx2] == 'e' || word[idx2] == 'i' || word[idx2] == 'o' || word[idx2] == 'u')
+					++vowel;
+				else ++consonant;
+				code += word[idx2];
+			}			
+		}
+		cout << '\n';
+		if (vowel >= 1 && consonant >= 2 && vowel + consonant == L)
+			answer.insert(code);
+	}
+
+	for (it = answer.begin(); it != answer.end(); ++it)
+		cout << *it << '\n';
+}
