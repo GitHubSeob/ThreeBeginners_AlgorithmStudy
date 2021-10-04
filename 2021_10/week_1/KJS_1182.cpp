a0445e0cf6c559e53905f732ebeae19321afd864
GitHubSeob <mnrz100@naver.com>
2021-10-04 22:14:41 +0900

    백준/1182/김진섭

---
+++ 2021_10/week_1/KJS_1182.cpp
@@ -0,0 +1,42 @@
+﻿//문제풀이: https://githubseob.tistory.com/126
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, S;
+vector<int>seq;
+vector<int>visit;
+int answer;
+
+void DFS(int idx) {
+	int idx2(0);
+	if (idx == N - 1 && visit.size() != 0) {
+		int sum(0);
+		for (idx2 = 0; idx2 < visit.size(); ++idx2)
+			sum += visit[idx2];
+		if (sum == S) ++answer;
+		return;
+	}
+	if (idx + 1 < N) {
+		visit.push_back(seq[idx + 1]);
+		DFS(idx + 1);
+		visit.pop_back();
+		DFS(idx + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> S;
+	int idx(0);
+	seq = vector<int>(N, 0);
+	for (idx = 0; idx < N; ++idx)
+		cin >> seq[idx];
+
+	DFS(0);
+	visit.push_back(seq[0]);
+	DFS(0);
+	cout << answer;
+}
