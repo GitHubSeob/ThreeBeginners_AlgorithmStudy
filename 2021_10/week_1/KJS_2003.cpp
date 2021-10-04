c68f6b7abd571ad770570537176bb37fd7dfe4bf
GitHubSeob <mnrz100@naver.com>
2021-10-04 22:16:11 +0900

    백준/2003/김진섭

---
+++ 2021_10/week_1/KJS_2003.cpp
@@ -0,0 +1,33 @@
+﻿//문제풀이: https://githubseob.tistory.com/127
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, M;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	int idx(0);
+	int idx2(0);
+	int answer(0);
+	vector<int>seq(N, 0);
+
+	for (idx = 0; idx < N; ++idx)
+		cin >> seq[idx];
+
+	for (idx = 0; idx < N; ++idx) {
+		int sum(0);
+		for (idx2 = idx; idx2 < N; ++idx2) {
+			sum += seq[idx2];
+			if (sum == M) {
+				++answer;
+				break;
+			}
+			else if (sum > M) break;
+		}
+	}
+	cout << answer;
+}
