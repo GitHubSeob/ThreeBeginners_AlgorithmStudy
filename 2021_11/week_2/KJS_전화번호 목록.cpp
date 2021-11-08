50dcefdc38025bc6d299d9482bc2269a8b541d2c
GitHubSeob <mnrz100@naver.com>
2021-11-08 22:20:20 +0900

    프로그래머스/전화번호 목록/김진섭

---
+++ 2021_11/week_2/KJS_전화번호 목록.cpp
@@ -0,0 +1,17 @@
+﻿//문제풀이: https://githubseob.tistory.com/139
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+bool solution(vector<string> phone_book) {
+    int idx(0), idx2(0);
+    sort(phone_book.begin(), phone_book.end());
+    for (idx = 0; idx < phone_book.size() - 1; ++idx)
+        if (phone_book[idx] == phone_book[idx + 1].substr(0, phone_book[idx].size()))
+            return false;
+    return true;
+}
