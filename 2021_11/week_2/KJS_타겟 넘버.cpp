50ad3b80c936b07c701c9421654dc4400ac61dc4
GitHubSeob <mnrz100@naver.com>
2021-11-08 22:20:31 +0900

    프로그래머스/타겟 넘버/김진섭

---
+++ 2021_11/week_2/KJS_타겟 넘버.cpp
@@ -0,0 +1,22 @@
+﻿//문제풀이: https://githubseob.tistory.com/138
+
+#include <string>
+#include <vector>
+
+using namespace std;
+int answer;
+
+void dfs(int sum, int num, vector<int>numbers, int target) {
+    if (num == numbers.size() && sum == target) answer++;
+    if (num < numbers.size()) {
+        dfs(sum + numbers[num], num + 1, numbers, target);
+        dfs(sum - numbers[num], num + 1, numbers, target);
+    }
+}
+
+int solution(vector<int> numbers, int target) {
+    int num(0);
+    int sum(0);
+    dfs(0, 0, numbers, target);
+    return answer;
+}
