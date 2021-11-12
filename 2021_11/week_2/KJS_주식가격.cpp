66030576d8b363365a5b8e11edc8b869f4671053
GitHubSeob <mnrz100@naver.com>
2021-11-12 23:55:43 +0900

    프로그래머스/주식가격/김진섭

---
+++ 2021_11/week_2/KJS_주식가격.cpp
@@ -0,0 +1,24 @@
+﻿//문제풀이: https://githubseob.tistory.com/141
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int>answer;
+    int idx(0);
+    int idx2(0);
+    for (idx = 0; idx + 1 < prices.size(); ++idx) {
+        for (idx2 = idx + 1; idx2 < prices.size(); ++idx2) {
+            if (prices[idx] > prices[idx2]) {
+                answer.push_back(idx2 - idx);
+                break;
+            }
+        }
+        if (idx2 == prices.size())
+            answer.push_back(idx2 - (idx + 1));
+    }
+    answer.push_back(0);
+    return answer;
+}
