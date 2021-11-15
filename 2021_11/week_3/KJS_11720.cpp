c020a45afbdbc30d3a6b0d3450c06f480e641be5
GitHubSeob <mnrz100@naver.com>
2021-11-15 22:50:30 +0900

    백준/11720/김진섭

---
+++ 2021_11/week_3/KJS_11720.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int num(0), idx(0);
+	cin >> num;	
+	int sum(0);
+	char number(' ');
+	
+	for (idx = 0; idx < num; ++idx) {
+		cin >> number;
+		sum += number-'0';
+	}
+	cout << sum;
+}
