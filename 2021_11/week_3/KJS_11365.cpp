85b751dd728947dc500751831530bea2c98b3c17
GitHubSeob <mnrz100@naver.com>
2021-11-15 22:50:36 +0900

    백준/11365/김진섭

---
+++ 2021_11/week_3/KJS_11365.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int idx(0);
+	string code("");
+
+	getline(cin, code);
+	while (code != "END") { // END를 입력할 때까지 반복
+		for (idx = 0; idx < code.size(); ++idx)
+			cout << code[code.size() - (idx + 1)];
+		cout << '\n';
+		getline(cin, code);
+	}
+}
