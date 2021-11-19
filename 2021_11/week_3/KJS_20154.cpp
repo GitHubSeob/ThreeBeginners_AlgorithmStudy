bb1f167d9beef7f69e53b4452ae8a79eec6b10e9
GitHubSeob <mnrz100@naver.com>
2021-11-19 18:33:58 +0900

    백준/20154/김진섭

---
+++ 2021_11/week_3/KJS_20154.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	vector<int>cnt = { 3,2,1,2,3,3,3,3,1,1,3,1,3,3,1,2,2,2,1,2,1,1,2,2,2,1 };
+	string s("");
+	cin >> s;
+	int idx(0);
+	int sum(0);
+	for (idx = 0; idx < s.size(); ++idx)
+		sum += cnt[s[idx]-65];
+	
+	if (sum % 2 == 1) cout << "I'm a winner!";
+	else cout << "You're the winner?";
+}
