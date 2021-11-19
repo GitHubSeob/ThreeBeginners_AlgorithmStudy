2455b24f247b92509b0dccd3881aac3ad0afc262
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2021-11-19 18:32:56 +0900

    백준/11365/김동균

---
+++ 2021_11/week_3/KDK_11365.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+	vector<string> v;
+
+	while (1) {
+		string s, tmp;
+		getline(cin, s);
+
+		if (s == "END")
+			break;
+		else {
+			for (int i = s.size() - 1; i >= 0; i--) {
+				tmp += s[i];
+			}
+		}
+		v.push_back(tmp);
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
+}
