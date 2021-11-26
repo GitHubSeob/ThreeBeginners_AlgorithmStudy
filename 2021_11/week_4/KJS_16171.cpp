f31863f2f35d7123e18f851501279cd6b3dc2ef5
GitHubSeob <mnrz100@naver.com>
2021-11-26 17:45:46 +0900

    백준/16171/김진섭

---
+++ 2021_11/week_4/KJS_16171.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int idx(0);
+	string s1(""), s2("");
+	cin >> s1 >> s2;
+	string answer("");
+	for (idx = 0; idx < s1.size(); ++idx) {
+		if (s1[idx] >= '0' && s1[idx] <= '9');
+		else answer += s1[idx];
+	}
+	if (answer.find(s2) != string::npos) cout << 1;
+	else cout << 0;	
+}
