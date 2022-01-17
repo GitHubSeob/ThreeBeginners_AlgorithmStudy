763491d52fccff99d267c21cc4f97f691d6c5cdc
GitHubSeob <mnrz100@naver.com>
2022-01-17 22:54:06 +0900

    백준/1436/김진섭

---
+++ 2022_01/week_3/KJS_1436.cpp
@@ -0,0 +1,26 @@
+﻿//문제풀이: https://githubseob.tistory.com/145
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N(0);
+	int idx(0);
+	int cnt(0);
+	cin >> N;	
+	string s_num("666");	
+
+	while (cnt<N) {
+		for (idx = 0; idx+2 < s_num.size(); ++idx) {
+			if (s_num[idx] == '6'&&s_num[idx+1]=='6'&&s_num[idx+2]=='6') {
+				cnt++;
+				break;
+			}
+		}
+		if(cnt!=N) s_num = to_string(stoi(s_num) + 1);
+	}
+	cout << s_num;
+}
