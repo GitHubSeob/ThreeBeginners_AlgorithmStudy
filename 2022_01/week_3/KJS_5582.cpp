9f10a3a2cdf4bbcdba9589e31a46ed9e7db178d5
GitHubSeob <mnrz100@naver.com>
2022-01-17 22:54:19 +0900

    백준/5582/김진섭

---
+++ 2022_01/week_3/KJS_5582.cpp
@@ -0,0 +1,38 @@
+﻿//문제풀이: https://githubseob.tistory.com/144
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s1(""), s2("");	
+	string s("");
+	cin >> s1 >> s2;
+	int answer(0);
+	int idx1(0), idx2(0);
+	
+	int start(0);
+	int end(0);
+
+	s = s1[0];
+	while (end <= s1.size() && start<s1.size()) {		
+		if (start > end) {
+			end = start;			
+			s = s1[start];
+		}
+		else if (s2.find(s) != string::npos) {
+			answer = max(answer, end - start + 1);
+			end++;
+			s += s1[end];
+		}
+		else {
+			start++;
+			if (!s.empty())
+				s.erase(0, 1);
+		}
+	}
+	cout << answer;
+}
