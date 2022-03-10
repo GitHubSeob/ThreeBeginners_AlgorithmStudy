ed5a50ef690ee8ab266d4551a423fd98ecc04fca
GitHubSeob <githubseob@gmail.com>
2022-03-10 18:22:05 +0900

    백준/2164/김진섭

---
+++ 2022_03/week_2/KJS_2164.cpp
@@ -0,0 +1,24 @@
+﻿//문제풀이: https://githubseob.tistory.com/155
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	queue<int>card;
+	int N(0),idx(0);
+	cin >> N;
+	for (idx = 1; idx <= N; ++idx)
+		card.push(idx);
+	
+	while (card.size() > 1) {
+		card.pop();
+		card.push(card.front());
+		card.pop();
+	}
+
+	cout << card.front();
+}
