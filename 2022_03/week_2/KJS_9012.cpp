2d354549d4d6c12a2e031d1a96e71207d31ed581
GitHubSeob <githubseob@gmail.com>
2022-03-10 18:22:14 +0900

    백준/9012/김진섭

---
+++ 2022_03/week_2/KJS_9012.cpp
@@ -0,0 +1,26 @@
+﻿//문제풀이: https://githubseob.tistory.com/154
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T(0), idx(0);
+	cin >> T;
+	
+	while (T--) {
+		stack<int>bracket;
+		string input("");
+		cin >> input;
+		for (idx = 0; idx < input.size(); ++idx) {
+			if (!bracket.empty() && bracket.top() == '(' && input[idx] == ')')
+				bracket.pop();
+			else bracket.push(input[idx]);
+		}
+		if (bracket.empty()) cout << "YES\n";
+		else cout << "NO\n";
+	}
+}
