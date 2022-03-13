86a50128c043b5251e30ba9ff7e9c3df1bbc5f21
GitHubSeob <githubseob@gmail.com>
2022-03-13 22:51:09 +0900

    백준/10799/김진섭

---
+++ 2022_03/week_2/KJS_10799.cpp
@@ -0,0 +1,32 @@
+﻿//문제풀이: https://githubseob.tistory.com/160
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string input("");
+	cin >> input;
+	int pipe(0), idx(0);
+	int answer(0);
+
+	for (idx = 0; idx < input.size(); ++idx) {
+		if (input[idx] == '(') {
+			if (input[idx + 1] == ')') {
+				answer += pipe;
+				++idx;
+			}
+			else {
+				pipe++;
+			}
+		}
+		else {
+			if (pipe > 0)
+				pipe--;
+			answer += 1;
+		}
+	}
+	cout << answer;
+}
