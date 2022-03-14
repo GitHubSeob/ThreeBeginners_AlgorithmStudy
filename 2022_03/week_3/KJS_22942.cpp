a59bc8da1deacc940e46f269fe93d2ea90e5acd7
GitHubSeob <githubseob@gmail.com>
2022-03-15 02:02:25 +0900

    백준/22942/김진섭

---
+++ 2022_03/week_3/KJS_22942.cpp
@@ -0,0 +1,47 @@
+﻿//문제풀이: https://githubseob.tistory.com/164
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N(0), idx(0), idx2(0);
+	cin >> N;
+
+	vector<pair<int, int>>data(N, { 0,0 });
+	int x(0), r(0);
+
+	for (idx = 0; idx < N; ++idx) {
+		cin >> x >> r;
+		data[idx].first = x - r;
+		data[idx].second = x + r;
+	}
+	sort(data.begin(), data.end());
+
+	for (idx = 0; idx + 1 < N; ++idx) {
+		for (idx2 = idx + 1; idx2 < N; ++idx2) {
+			if (data[idx].first == data[idx2].first) {
+				if (data[idx].second != data[idx2].second) {
+					cout << "NO";
+					return 0;
+				}
+				else continue;
+			}
+			else if (data[idx].second < data[idx2].first) {
+				break;
+			}
+			else if (data[idx2].second < data[idx].second) {
+				continue;
+			}
+			else {
+				cout << "NO";
+				return 0;
+			}
+		}
+	}
+	cout << "YES";
+}
