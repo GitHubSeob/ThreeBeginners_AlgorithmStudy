66ae9a5559990ae615124e6ef6c5bf7a49b853fa
KJS <83795383+GitHubSeob@users.noreply.github.com>
2022-03-18 17:16:03 +0900

    백준/2493/김진섭

---
+++ 2022_03/week_3/KJS_2493.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N(0), idx(0);
+	cin >> N;
+
+	vector<int>tower(N, 0);
+	vector<int>answer(N, 0);
+	stack<int>s;
+
+	for (idx = 0; idx < N; ++idx)
+		cin >> tower[idx];
+
+	s.push({ 0 });
+	for (idx = 1; idx < N; ++idx) {
+		if (tower[s.top()] >= tower[idx]) {
+			answer[idx] = s.top() + 1;
+		}
+		else {
+			while (s.size() > 1) {
+				s.pop();
+				if (tower[s.top()] >= tower[idx]) {
+					answer[idx] = s.top() + 1;
+					break;
+				}
+			}
+		}
+		s.push(idx);
+	}
+	for (idx = 0; idx < N; ++idx)
+		cout << answer[idx] << ' ';
+}
