43f79a3d36cc15ac78f25453e6e2851fdf453480
GitHubSeob <githubseob@gmail.com>
2022-03-21 04:07:51 +0900

    백준/14425/김진섭

---
+++ 2022_03/week_4/KJS_14425.cpp
@@ -0,0 +1,27 @@
+﻿//문제풀이: https://githubseob.tistory.com/169
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	unordered_map<string, int>set;
+	int N(0), M(0), idx(0), answer(0);
+	cin >> N >> M;
+
+	string s;
+	for (idx = 0; idx < N; ++idx) {
+		cin >> s;
+		set.insert({ s,1 });
+	}
+
+	for (idx = 0; idx < M; ++idx) {
+		cin >> s;
+		answer += set[s];
+	}
+
+	cout << answer;
+}
