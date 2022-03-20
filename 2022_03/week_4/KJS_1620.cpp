3ded0cfd3a870df39abc66d01d212273de917c8f
GitHubSeob <githubseob@gmail.com>
2022-03-21 03:04:08 +0900

    백준/1620/김진섭

---
+++ 2022_03/week_4/KJS_1620.cpp
@@ -0,0 +1,34 @@
+﻿//문제풀이: https://githubseob.tistory.com/168
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N(0), M(0), idx(0);
+	string input("");
+
+	cin >> N >> M;
+
+	unordered_map<string, int>pokemonS;
+	unordered_map<int, string>pokemonI;
+
+	for (idx = 1; idx <= N; ++idx) {
+		cin >> input;
+		pokemonS.insert({ input,idx });
+		pokemonI.insert({ idx, input });
+	}
+
+	while (M--) {
+		cin >> input;
+		if (input[0] >= '0' && input[0] <= '9')
+			cout << pokemonI[stoi(input)];
+
+		else cout << pokemonS[input];
+		cout << '\n';
+	}
+}
