e8e5a43ac01e6d0697924911a1bd261769ae5b5e
GitHubSeob <githubseob@gmail.com>
2022-03-30 13:59:18 +0900

    백준/2075/김진섭

---
+++ 2022_03/week_5/KJS_2075.cpp
@@ -0,0 +1,26 @@
+﻿//문제풀이: https://githubseob.tistory.com/173
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int idx(0), N(0), num(0);
+	priority_queue<int, vector<int>, greater<int>>pq;
+	cin >> N;
+
+	for (idx = 0; idx < N; ++idx) {
+		cin >> num;
+		pq.push(num);
+	}
+	for (idx; idx < N * N; ++idx) {
+		cin >> num;
+		pq.push(num);
+		pq.pop();
+	}
+
+	cout << pq.top();
+}
