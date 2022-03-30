bb873c98590edaa34dfe16ff3a916e0237a2f3d8
GitHubSeob <githubseob@gmail.com>
2022-03-30 13:59:34 +0900

    백준/7662/김진섭

---
+++ 2022_03/week_5/KJS_7662.cpp
@@ -0,0 +1,48 @@
+﻿//문제풀이: https://githubseob.tistory.com/172
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T(0), k(0), n(0);
+	char oper(' ');
+	cin >> T;
+
+	while (T--) {
+		cin >> k;
+		map<int, int>m;
+		map<int, int>::iterator iter;
+		while (k--) {
+			cin >> oper >> n;
+			if (oper == 'I') {
+				m[n]++;
+			}
+			else {
+				if (!m.empty()) {
+					if (n == 1) {
+						iter = --m.end();						
+						if (iter->second == 1)
+							m.erase(iter);
+						else iter->second--;
+					}
+					else {
+						iter = m.begin();
+						if (iter->second == 1)
+							m.erase(iter);
+						else iter->second--;
+					}
+				}
+			}
+		}
+
+		if (m.empty()) cout << "EMPTY\n";
+		else {
+			cout << (--m.end())->first << " ";
+			cout << m.begin()->first << '\n';
+		}
+	}
+}
