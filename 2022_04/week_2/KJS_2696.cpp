1ab135be2dd4e2010cf265bcc3a7bf0693586a4a
GitHubSeob <githubseob@gmail.com>
2022-04-12 19:06:47 +0900

    백준/2696/김진섭

---
+++ 2022_04/week_2/KJS_2696.cpp
@@ -0,0 +1,40 @@
+﻿//문제풀이: https://githubseob.tistory.com/178
+
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T(0), M(0);
+	int idx(0), idx2(0), num(0);
+	cin >> T;
+
+	while (T--) {
+		cin >> M;
+		multiset<int>ms;
+		multiset<int>::iterator iter;
+		vector<int>answer;
+
+		for (idx = 0; idx < M; ++idx) {
+			cin >> num;
+			ms.insert(num);
+			iter = ms.begin();
+			if (idx % 2 == 0) {
+				for (idx2 = 0; idx2 < ms.size() / 2; ++idx2) {
+					iter++;
+				}
+				answer.push_back(*iter);
+			}
+		}
+		cout << answer.size() << '\n';
+		for (idx = 0; idx < answer.size(); ++idx) {
+			cout << answer[idx] << ' ';
+		}
+		cout << '\n';
+	}
+
+}
